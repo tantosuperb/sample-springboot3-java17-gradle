3fd68c9b66e7b55b2922e14569d99198b7924f28
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2023-09-13 19:54:20 +0900

    feat: 프로젝트 초기 구성

---
+++ src/main/java/com/example/samplespringboot3javagradle/SampleSpringBoot3JavaGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringboot3javagradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBoot3JavaGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringBoot3JavaGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplespringboot3javagradle/SampleSpringBoot3JavaGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringboot3javagradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBoot3JavaGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
